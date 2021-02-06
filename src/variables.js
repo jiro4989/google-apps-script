fa7cee0b2074890226c61d839198b898940eced0
jiro4989 <jiroron666@gmail.com>
2021-02-07 00:39:50 +0900

    add setProperty

--- src/variables.js
+++ src/variables.js
@@ -16,3 +16,12 @@ const userName = props.getProperty('USER_NAME');
  * @type {String}
  */
 const twitterUserId = props.getProperty('TWITTER_USER_ID');
+
+function setProperties() {
+  props.setProperty('WEATHER_SEARCH_CITY', '');
+  props.setProperty('WEATHER_SEARCH_COUNTRY', '');
+  props.setProperty('USER_NAME', '');
+  props.setProperty('OPEN_WEATHER_MAP_API_KEY', '');
+  props.setProperty('TWITTER_API_KEY', '');
+  props.setProperty('TWITTER_API_SECRET', '');
+}
